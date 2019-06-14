ae3361340f9a249371b49be130958fb6d6b4c7b6
esimov <esimovaq@yahoo.com>
2019-06-14 14:18:41 +0300

    create build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+VERSION="1.1.0"
+PROTECTED_MODE="no"
+
+export GO15VENDOREXPERIMENT=1
+export GO111MODULE=on
+
+cd $(dirname "${BASH_SOURCE[0]}")
+OD="$(pwd)"
+
+# build and store objects into original directory.
+go build -mod=vendor -ldflags "-X main.Version=$VERSION" -o "$OD/colidr" cli/main.go
+
+if [ -d $GOPATH ]
+then
+    cp colidr $GOPATH/bin
+else
+    cp colidr /usr/local/bin
+fi
+
+rm $OD/colidr
+
